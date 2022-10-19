cb61db47658297bde142e929ff41bafef29fe352
Кирилл Соснюк <kirill-sosnyuk@yandex.ru>
2022-10-20 00:32:02 +0300

    Name=Main

--- task2,4.py
+++ task2,4.py
@@ -22,6 +22,6 @@ nested_list = [
 	['d', 'e', 'f', 'h', False],
 	[1, 2, None],
 ]
-
-for item in flat_generator(nested_list):
-	print(item)
+if __name__ == '__main__':
+    for item in flat_generator(nested_list):
+        print(item)
